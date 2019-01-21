bbe1ea696d809286f991ea624403f0832d31988d
ni851ste <ni851ste@htwg-konstanz.de>
2019-01-21 14:19:20 +0100

    slow but steady

--- idea_project/src/TUI.java
+++ idea_project/src/TUI.java
@@ -1,7 +1,38 @@
+import java.sql.*;
+import java.io.Console;
+
+
 public class TUI {
-public static void main (String[] args){
-    System.out.println("test");
-}
+
+    public static void main (String[] args){
+        try {
+           /* Class.forName("oracle.jdbc.driver.OracleDriver");
+            String url = "jdbc:oracle:thin:@oracle12c.in.htwgkonstanz.de:1521:ora12c";
+            Connection conn = DriverManager.getConnection(url, "dbsys08", "NiggySchiggy");
+            Statement stmt = conn.createStatement();
+            ResultSet rs1 = stmt.executeQuery("SELECT mail, kundennummer FROM benutzer");
+*/
+            Console console = System.console();
+            String input = console.readLine("user: ");
+            String[] splitStr = input.split("\\s+");
+            System.out.println(splitStr);
+
+
+/*            while (rs1.next()) {
+                String r1 = rs1.getString("name");
+                int r2 = rs1.getInt("gehalt");
+                System.out.println(r1 + " verdient " + r2);
+            }
+            rs1.close(); stmt.close(); conn.close();
+*/
+
+
+        } catch (Exception e){
+            System.out.println("fehler: " + e);
+        }
+
+
+    }
 
 
 }
